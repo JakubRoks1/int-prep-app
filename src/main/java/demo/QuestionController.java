e9faa1d9aa7ebb091947e6f2f6f13522228d5a76
Jakub Roks <kubaroks@onet.eu>
2024-03-01 13:35:17 +0100

    Initial Commit

---
+++ src/main/java/demo/QuestionController.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @GetMapping("AllQuestions")
+    public String getAllQuestions(){
+        return "Your questions";
+    }
+}
