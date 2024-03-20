948734ef9391507a10825ab22df8668507aecc02
Jakub Roks <kubaroks@onet.eu>
2024-03-20 21:06:37 +0100

    Implement method for submitting quiz responses

--- src/main/java/demo/controller/QuizController.java
+++ src/main/java/demo/controller/QuizController.java
@@ -2,6 +2,7 @@ package demo.controller;
 
 import demo.model.Question;
 import demo.model.QuestionWrapper;
+import demo.model.Response;
 import demo.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +33,10 @@ public class QuizController {
     @GetMapping("get/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
         return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses) {
+        return quizService.calculateResult(id, responses);
     }
 }
--- src/main/java/demo/service/QuizService.java
+++ src/main/java/demo/service/QuizService.java
@@ -5,6 +5,7 @@ import demo.dao.QuizDAO;
 import demo.model.Question;
 import demo.model.QuestionWrapper;
 import demo.model.Quiz;
+import demo.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,5 +46,19 @@ public class QuizService {
         }
 
         return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        Quiz quiz = quizDAO.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int right = 0;
+        int i = 0;
+        for (Response response : responses) {
+            if (response.getResponse().equals(questions.get(i).getCorrectanswer()));
+            right++;
+
+            i++;
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
     }
 }
